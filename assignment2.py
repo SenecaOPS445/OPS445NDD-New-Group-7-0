f350955c7281d396346c6d4e78cef26e5e57018b
Manik Shrestha <mshrestha8@myseneca.ca>
2025-04-03 17:21:04 -0400

    Basic setup and added network fetching function

--- assignment2.py
+++ assignment2.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+
+import subprocess
+import sys
+
+def get_network_config():
+    """ Display the current network configuration."""
+    try:
+        result = subprocess.run(['ip', 'a'], capture_output=True, text=True, check=True)
+        print(result.stdout)
+    except subprocess.CalledProcessError:
+        print("Error fetching network config.")
+        sys.exit(1)
+
+
+
+if __name__ == "__main__":
+    get_network_config()
